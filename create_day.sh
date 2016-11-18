dd2b0afc54813c68befdb8dbae92ce92f8cd0cd3
Paul Wicks <pwicks86@gmail.com>
2016-11-17 21:01:54 -0800

    create_day.sh added

---
+++ create_day.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+day=$1
+
+day_len=${#day} 
+
+if [[ $day_len == 1 ]]
+then
+    dir_name=day0$1
+else
+    dir_name=day$1
+fi
+
+mkdir $dir_name
+echo "f = open('input.txt')" > $dir_name/p1.py
+echo "f = open('input.txt')" > $dir_name/p2.py
+
+wget -O $dir_name/input.txt http://adventofcode.com/2016/$day/input
